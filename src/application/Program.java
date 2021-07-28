a3a49fade4b7371e711d64b96fa75ee676274a52
Israel Nascimento <isra.nascimento@gmail.com>
2021-07-27 23:02:45 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerSao();
 		
@@ -41,5 +44,13 @@ public class Program {
 		seller.setName("Martha waine");
 		sellerDao.update(seller);
 		System.out.println("update completed");
+		
+		System.out.println("\n=== TEST 6: Seller delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
 }
