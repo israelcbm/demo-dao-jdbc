94d15e30c89a2ac4b4a92565aad89b09d06f7b76
Israel Nascimento <isra.nascimento@gmail.com>
2021-07-27 22:51:34 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,5 +35,11 @@ public class Program {
 		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
-		}
+	
+		System.out.println("\n=== TEST 5: Seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha waine");
+		sellerDao.update(seller);
+		System.out.println("update completed");
 	}
+}
